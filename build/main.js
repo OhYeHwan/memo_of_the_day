fc868870ed53f0196105cd3b5bc27c46d0561844
OOHYEE <azss0470@gmail.com>
2021-11-19 18:26:26 +0900

    Feat : Video, Image, Note, Task 버튼 클릭 시 팝업기능 추가

--- build/main.js
+++ build/main.js
@@ -1,3 +1,32 @@
 "use strict";
-console.log("hello");
+const popup = document.querySelector(".popup");
+const url = document.querySelector(".popup__box__url");
+const content = document.querySelector(".popup__box__content");
+const titleInput = document.querySelector(".popup__box__input__title");
+const urlInput = document.querySelector(".popup__box__url__input");
+const contentInput = document.querySelector(".popup__box__content__input");
+function openUrlPopUp(event) {
+    url.style.display = "flex";
+    content.style.display = "none";
+    openPopUp(event);
+}
+function openContentPopUp(event) {
+    content.style.display = "flex";
+    url.style.display = "none";
+    openPopUp(event);
+}
+function openPopUp(event) {
+    event.preventDefault();
+    popup.style.display = "flex";
+}
+function closePopUp(event) {
+    event.preventDefault();
+    inputClear();
+    popup.style.display = "none";
+}
+function inputClear() {
+    titleInput.value = "";
+    urlInput.value = "";
+    contentInput.value = "";
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
-console.log("hello");
+const popup = document.querySelector(".popup") as HTMLDivElement;
+const url = document.querySelector(".popup__box__url") as HTMLDivElement;
+const content = document.querySelector(
+  ".popup__box__content"
+) as HTMLDivElement;
+const titleInput = document.querySelector(
+  ".popup__box__input__title"
+) as HTMLInputElement;
+const urlInput = document.querySelector(
+  ".popup__box__url__input"
+) as HTMLInputElement;
+const contentInput = document.querySelector(
+  ".popup__box__content__input"
+) as HTMLInputElement;
+
+function openUrlPopUp(event: Event) {
+  url.style.display = "flex";
+  content.style.display = "none";
+  openPopUp(event);
+}
+
+function openContentPopUp(event: Event) {
+  content.style.display = "flex";
+  url.style.display = "none";
+  openPopUp(event);
+}
+
+function openPopUp(event: Event) {
+  event.preventDefault();
+  popup.style.display = "flex";
+}
+
+function closePopUp(event: Event) {
+  event.preventDefault();
+  inputClear();
+  popup.style.display = "none";
+}
+
+function inputClear() {
+  titleInput.value = "";
+  urlInput.value = "";
+  contentInput.value = "";
+}
