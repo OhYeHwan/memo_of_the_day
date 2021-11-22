79a0affd5d9d24abe7053660f91e7cd6600b3984
OOHYEE <azss0470@gmail.com>
2021-11-22 22:05:00 +0900

    Rename : components, style 폴더 생성 및 파일 이동

---
+++ src/components/dialog/dialog.ts
@@ -0,0 +1,44 @@
+import { BaseComponent, Component } from "../component.js";
+
+type OnCloseListener = () => void;
+type OnSubmitListener = () => void;
+
+export class InputDialog extends BaseComponent<HTMLElement> {
+  closeListener?: OnCloseListener;
+  submitListener?: OnSubmitListener;
+  constructor() {
+    super(`
+            <dialog class="dialog">
+                <div class="dialog__container">
+                    <button class="close">&times;</button>
+                    <div id="dialog__body"></div>
+                    <button class="dialog__submit">ADD</button>
+                </div>
+            </dialog>
+        `);
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
+
+    const submitBtn = this.element.querySelector(
+      ".dialog__submit"
+    )! as HTMLButtonElement;
+    submitBtn.onclick = () => {
+      this.submitListener && this.submitListener();
+    };
+  }
+
+  setOnCloseListener(listner: OnCloseListener) {
+    this.closeListener = listner;
+  }
+
+  setOnSubmitListener(listner: OnSubmitListener) {
+    this.submitListener = listner;
+  }
+
+  addChild(child: Component) {
+    const body = this.element.querySelector("#dialog__body")! as HTMLElement;
+    child.attachTo(body);
+  }
+}
